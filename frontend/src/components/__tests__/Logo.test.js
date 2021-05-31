88f1aec219947e8b9b422bffc7d7601b65b5815a
Vlada FKZ Laptop <vlada030@gmail.com>
2021-05-31 14:56:07 +0200

    logo test - pokusaj testiranja media queries

--- frontend/src/components/__tests__/Logo.test.js
+++ frontend/src/components/__tests__/Logo.test.js
@@ -1,16 +1,31 @@
+// promena widthai heighta window objekta
+import {resizeTo} from 'window-resizeto'
+import { render, screen } from "@testing-library/react"
 import {BrowserRouter} from 'react-router-dom'
-import { render, screen } from "@testing-library/react";
-import Logo from "../Logo";
-import { GlobalProvider } from "../../context/global_context";
+import Logo from "../Logo"
+import { GlobalProvider } from "../../context/global_context"
+
 
 test("Button for sidebar opening is rendered", () => {
-    render(
+
+    // resizeTo(window, 1920, 1080)
+    // resizeTo(window, 500, 108)
+    
+    const {getByTestId} = render(
         <GlobalProvider>
             <BrowserRouter>
-            <Logo />    
-
+                <Logo />    
             </BrowserRouter>
         </GlobalProvider>
     );
-    expect(screen.queryByTestId("svg")).toBeFalsy();
+
+    console.log({width: window.innerWidth, height: window.innerHeight});
+
+    //screen.debug()
+
+    //expect(getByTestId("svg")).toBeVisible()
+    //expect(screen.getByRole('button')).toBeInTheDocument()
+    expect(getByTestId("svg")).toHaveStyle({display: none})
+
+
 });
---
+++ frontend/src/setupTests.js
@@ -0,0 +1,5 @@
+// react-testing-library renders your components to document.body,
+// da nebi u svakom fajlu ovo dodavali kreira se setupTests.js
+// koji se pokrece pre testiranja
+
+import '@testing-library/jest-dom';
